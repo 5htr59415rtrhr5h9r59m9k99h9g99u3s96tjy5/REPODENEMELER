bd93676a43e09d585e4b1c22706d940e7dcc125e
5htr59415rtrhr5h9r59m9k99h9g99u3s96tjy5 <saplayanzimeto@gmail.com>
2026-08-29 23:30:39 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// 55 Fındık — Firebase Ayarları
+export const firebaseConfig = {
+  apiKey: "AIzaSyDhrKkXyTj6s0krVd8y2Q69zmZMwcmREgU",
+  authDomain: "findik-b97d2.firebaseapp.com",
+  projectId: "findik-b97d2",
+  storageBucket: "findik-b97d2.firebasestorage.app",
+  messagingSenderId: "699282955290",
+  appId: "1:699282955290:web:743097c20a389a50a445a3",
+  measurementId: "G-W47L0HHLYS"
+};
